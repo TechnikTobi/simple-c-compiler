ebc3699c43472cc4578d16700b6e8365ad36fc04
Tobias Prisching <tobias.prisching@icloud.com>
2025-04-03 16:15:19 +0200

    Still buggy...
    For some reason, the first call of print_regs accesses the local variables of main() instead of the parameters that were given!

---
+++ example_inputs/simple_func_call.c
@@ -0,0 +1,109 @@
+int main()
+{
+    int a;
+    int b;
+
+    a = 130;
+    b = 124;
+
+    /* print('\n', 'a', '0', _A0, ' ', 's', 'p', _SP, ' ', 's', '0', _S0, '\n'); */
+    print_regs(_A0, _SP, _S0);
+
+    print('x', ' ');
+    spike_print(test(&b));
+
+    /* print('\n', 'a', '0', _A0, ' ', 's', 'p', _SP, ' ', 's', '0', _S0, '\n'); */
+    print_regs(_A0, _SP, _S0);
+
+    spike_print(a);
+
+    print('\n');
+    exit(0);
+}
+
+int test(int* n)
+{
+    
+    print_regs(_A0, _SP, _S0);
+
+    /*
+    print('y', ' ');
+    spike_print(*n);
+    print('t');
+    print('\n');
+    */
+
+    return 747;
+}
+
+void print_regs(int a0, sp, s0)
+{
+    print('\n', 'a', '0', '=');
+    spike_print_hex(a0);
+    print(' ', 's', 'p', '=');
+    spike_print_hex(sp);
+    print(' ', 's', '0', '=');
+    spike_print_hex(s0);
+    print('\n');
+}
+
+void spike_print_hex(int n)
+{
+    int i;
+    int c;
+
+    i = 32 - 4;
+
+    print('0', 'x');
+
+    while (i >= 0)
+    {
+        c = (n >> i) % 16;
+        if (c < 10)
+        {
+            print(c + 48);
+        }
+        else
+        {
+            print((c-10) + 65);
+        }
+
+        i -= 4;
+    }
+
+    return;
+}
+
+
+void spike_print(int n)
+{
+    int i;
+    char c;
+
+    if (n == 0)
+    {
+        print('0');
+        return;
+    }
+
+    if (n < 0)
+    {
+        n = -n;
+        print('-');
+    }
+
+    i = 1;
+    while (n >= i)
+    {
+        i *= 10;
+    }
+
+    while (i > 1)
+    {
+        i /= 10;
+        c = (n / i) % 10 + 48;
+        print(c);
+    }
+
+    return;
+}
